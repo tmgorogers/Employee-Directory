6ddf3c2e85fc990e0cfbac8655060333db4dc5fc
Tierra Rogers <tierragore@yahoo.com>
2021-05-31 22:13:59 -0400

    Updating Body.js

--- employee-directory/src/components/Body.js
+++ employee-directory/src/components/Body.js
@@ -16,8 +16,43 @@ const Body = () => {
     }
 
     return (
-        <tbody
-    )
-}
+        <tbody>
+            {context.developerState.filteredUsers[0] !== undefined &&
+            context.developerState.filteredUsers[0].name !== undefined ? (
+                context.developerState.filteredUsers.map(
+                   ({login, name, picture, phone, email, dob}) => {
+                       return (
+                           <tr key = {login.uuid}>
+                               <td data-th="Image" className="align-middle">
+                                   <img src={"picture image for " + name.first + " " + name.last}
+                                   className="img-responsive"
+                                   />
+                               </td>
+                               <td data-th="Name" className="name-cell align-middle">
+                                   {name.first} {name.last}
+                               </td>
+                               <td data-th="Phone" className="align-middle">
+                                   {phone}
+                               </td>
+                               <td data-th="Email" className="align-middle">
+                                   <a href={"mailto:" + email} target="_blank">
+                                       {email}
+                                   </a>
+                               </td>
+                               <td data-th="DOB" className="align-middle">
+                                   {formatDate(dob.date)}
+                               </td>
+                           </tr>
+                       );
+                   } 
+                )
+            ) 
+             (
+            <></>
+       
+        )}
+        </tbody>
+    );
+};
 
 export default Body;
